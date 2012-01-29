b708c5a1d8b1e5f34bddd8a2d45eae49126909a8
apaatsf <apaatsf@e6a6316d-f3cf-4a1b-a10c-5ca315db6d77>
2012-01-29 10:44:45 +0000

    Added IEC 103 driver

---
+++ trunk/src/drivers/iec103/iec103driverCommand.h
@@ -0,0 +1,33 @@
+/**********************************************************************
+--- Qt Architect generated file ---
+File: Iec103driverCommand.h
+Last generated: Mon May 22 17:14:04 2000
+*********************************************************************/
+#ifndef Iec103driverCommand_included
+#define Iec103driverCommand_included
+#include "iec103driverCommandData.h"
+
+//NOTA BENE: questa dialog viene caricata
+//dalla dll nel client di IndigoSCADA (ist.exe)
+
+class Iec103driverCommand : public Iec103driverCommandData
+{
+	Q_OBJECT
+	QString Unit_type; // receipe name
+	QString samplePointName;
+	enum {tUnit = 1
+	//	, tSerial
+	}; // transaction codes
+	public:
+	Iec103driverCommand(
+	QWidget *parent = NULL,
+	const char *name = NULL,
+	const char *unit_type = NULL );
+	virtual ~Iec103driverCommand();
+	protected slots:
+	virtual void Help();
+	virtual void OkClicked();
+	void QueryResponse (QObject *, const QString &, int, QObject*); // handles database responses
+};
+#endif // Iec103driverCommand_included
+
