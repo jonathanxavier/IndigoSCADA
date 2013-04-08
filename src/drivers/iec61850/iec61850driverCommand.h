5624770d2c9198ee784ee7007bb9b27005cacd37
apaatsf <apaatsf@e6a6316d-f3cf-4a1b-a10c-5ca315db6d77>
2013-04-08 14:32:19 +0000

    Added IEC 61850 protocol

---
+++ src/drivers/iec61850/iec61850driverCommand.h
@@ -0,0 +1,33 @@
+/**********************************************************************
+--- Qt Architect generated file ---
+File: Iec61850driverCommand.h
+Last generated: Mon May 22 17:14:04 2000
+*********************************************************************/
+#ifndef Iec61850driverCommand_included
+#define Iec61850driverCommand_included
+#include "iec61850driverCommandData.h"
+
+//NOTA BENE: questa dialog viene caricata
+//dalla dll nel client di IndigoSCADA (ist.exe)
+
+class Iec61850driverCommand : public Iec61850driverCommandData
+{
+	Q_OBJECT
+	QString Unit_type; // receipe name
+	QString samplePointName;
+	enum {tUnit = 1
+	//	, tSerial
+	}; // transaction codes
+	public:
+	Iec61850driverCommand(
+	QWidget *parent = NULL,
+	const char *name = NULL,
+	const char *unit_type = NULL );
+	virtual ~Iec61850driverCommand();
+	protected slots:
+	virtual void Help();
+	virtual void OkClicked();
+	void QueryResponse (QObject *, const QString &, int, QObject*); // handles database responses
+};
+#endif // Iec61850driverCommand_included
+
