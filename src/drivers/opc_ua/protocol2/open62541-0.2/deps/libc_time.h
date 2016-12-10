6c6f7791e1d7544a4176f2b7ceaa7ee4125b8102
apaatsf <apaatsf@e6a6316d-f3cf-4a1b-a10c-5ca315db6d77>
2016-12-10 14:38:30 +0000

    Update

--- src/drivers/opc_ua/protocol2/open62541-0.2/deps/libc_time.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBC_TIME_H_
-#define LIBC_TIME_H_
-
-#include <limits.h>
-#include <time.h>
-int __secs_to_tm(long long t, struct tm *tm);
-
-#endif /* LIBC_TIME_H_ */
--- src/drivers/opc_ua/protocol2/open62541-0.2/tests/testing_networklayers.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTING_NETWORKLAYERS_H_
-#define TESTING_NETWORKLAYERS_H_
-
-#include "ua_server.h"
-
-/** @brief Create the TCP networklayer and listen to the specified port */
-UA_Connection createDummyConnection(void);
-
-#endif /* TESTING_NETWORKLAYERS_H_ */
