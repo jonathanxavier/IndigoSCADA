5e14dd83005075fe04734e61b35d65f7f2ad95ff
apaatsf <apaatsf@e6a6316d-f3cf-4a1b-a10c-5ca315db6d77>
2018-04-19 14:00:44 +0000

    Update

--- src/drivers/opc_client_da/opc_client_da_instance.cpp
+++ src/drivers/opc_client_da/opc_client_da_instance.cpp
@@ -778,7 +778,11 @@ void Opc_client_da_Instance::get_items_from_local_fifo(void)
 		}
 
 		////////////////////////////////Send in gloabal monitor direction/////////////////
-		fifo_put(fifo_global_monitor_direction, (char *)p_item, sizeof(struct iec_item));	
+		//fifo_put(fifo_global_monitor_direction, (char *)p_item, sizeof(struct iec_item));
+		if(fifo_global_monitor_direction)
+		{
+			fifo_global_monitor_direction->put(p_item, sizeof(struct iec_item));
+		}
 		//////////////////////////////////////////////////////////////////////////////////
 		
 		QString sp_name;
