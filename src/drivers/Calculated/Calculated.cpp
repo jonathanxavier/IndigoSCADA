8f49a9ca5652f85ec79108c4776554a4b4944292
apaatsf <apaatsf@e6a6316d-f3cf-4a1b-a10c-5ca315db6d77>
2020-12-04 09:43:41 +0000

    Moved scripts in project folder

--- src/drivers/Calculated/Calculated.cpp
+++ src/drivers/Calculated/Calculated.cpp
@@ -292,14 +292,14 @@ void WorkerProc(void* pParam)
 	int err;
 	DWORD exitcode = 0;
 
-	//script.c si trova nella directory C:\scada\scripts
+	//script.c is in folder HOME\scada\project\scripts
 
 	const int nBufferSize = 500;
 	char scada_home_dir[nBufferSize+1];
 	strcpy(scada_home_dir, GetScadaHomeDirectory());
 	
 	char cline[520];
-	sprintf(cline, "-I%s\\scripts\\include -I%s\\scripts -D_EiC", scada_home_dir, scada_home_dir);
+	sprintf(cline, "-I%s\\project\\scripts\\include -I%s\\project\\scripts -D_EiC", scada_home_dir, scada_home_dir);
 	Calc("script.c", &err, cline); //start loop of interpreter
 
 	//_endthread();
